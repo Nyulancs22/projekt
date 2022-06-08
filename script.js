91aa782148367cdbdde9f8f685db0d0442e87cdc
Nyulancs22 <zibrinyijulia@gmail.com>
2022-06-08 17:23:49 +0200

    vizsgálat

---
+++ script.js
@@ -0,0 +1,36 @@
+let szamolas = function szamol (event) {
+    event.preventDefault();
+
+    let sz = document.getElementById('fname').value;
+
+    let k = sz % 2
+
+    let ha = sz % 3
+
+    let o = sz % 5
+
+    let he = sz % 7
+
+    if (k == 0) {
+        document.getElementById('megoldas').innerHTML = "Ez a szá prímszám."
+    } else {
+        if (ha == 0) {
+            document.getElementById('megoldas').innerHTML = "Ez a szá prímszám."            
+        } else {
+            if (o == 0) {
+                document.getElementById('megoldas').innerHTML = "Ez a szá prímszám."
+            } else {
+                if (he == 7) {
+                    document.getElementById('megoldas').innerHTML = "Ez a szá prímszám."
+                } else {
+                    document.getElementById('megoldas').innerHTML = "Ez a szá nem prímszám."
+                }
+            }
+        }
+    }
+
+}
+
+document.getElementById('szamol').addEventListener('click', szamolas)
+
+
